add5f3afb07f89c351e34ab681c39a169efca4f3
NingGuo19 <ningguo19@gmail.com>
2019-11-01 20:10:18 +1100

    1 file modified

--- src/app/heroes/heroes.service.ts
+++ src/app/heroes/heroes.service.ts
@@ -1,11 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 import { ConfigService } from '../config/config.service';
+import { Config } from '../config/config.service';
+
+import { Hero } from '../data/hero';
 
 @Injectable()
 export class HeroesService {
 
-  constructor(private configService: ConfigService) { }
+  config: Config;
+
+  constructor(private configService: ConfigService,
+              private http: HttpClient ) { }
+
+  getAPIUrl(){
+    this.configService.getConfig().subscribe(
+      (data: Config)=>this.config={...data}
+    );
+  }
+
+  getHeroes(): Observable<Hero[]>{
+    this.getAPIUrl();
+    return this.http.get<Hero[]>(this.config.heroesUrl);
+  }
 
 }
