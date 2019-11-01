46c25759bbebd06ba663f379743f21ff35b883b8
NingGuo19 <ningguo19@gmail.com>
2019-11-01 19:36:52 +1100

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -15,9 +18,15 @@ import { HeoresComponent } from './heores/heores.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {
+        dataEncapsulation: false,
+        passThruUnknownUrl: true,
+        put204: false // return entity after PUT/update
+      }
+    ) ],
   declarations: [ AppComponent, HelloComponent, FeatureDisplayComponent, StudentDisplayComponent, ConfigComponent, HeoresComponent, HeroesComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [StudentService, ConfigService]
+  providers: [StudentService, ConfigService, InMemoryDataService]
 })
 export class AppModule { }
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+@Injectable()
+export class InMemoryDataService implements InMemoryDbService{
+
+  createDb() {
+    const heroes = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+    ];
+    return {heroes};
+  }
+
+  constructor() { }
+
+}
