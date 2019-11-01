9e145b676e02e3e1dfbe8855a6c15fca9b032ac6
NingGuo19 <ningguo19@gmail.com>
2019-11-01 17:35:04 +1100

    4 files modified

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -5,11 +5,14 @@ import { ConfigService, Config } from './config.service';
   selector: 'app-config',
   templateUrl: './config.component.html',
   providers: [ ConfigService ],
-  styleUrls: ['./config.component.css']
+  styleUrls: ['./config.component.css'],
+  styles: ['.error {color: red;}']
 })
 export class ConfigComponent implements OnInit {
 
   config: Config;
+  error: any;
+  headers: string[];
 
   constructor(private configService: ConfigService ) { }
 
@@ -18,14 +21,42 @@ export class ConfigComponent implements OnInit {
 
   clear(){
     this.config = undefined;
+    this.error = undefined;
+    this.headers = undefined;
   }
 
-  showConfig(){
+  showConfig_1(){
     this.configService.getConfig()
     .subscribe((data: Config) => this.config = {
         heroesUrl: data['heroesUrl'],
         textfile:  data['textfile']
     });
+  }
+
+  showConfig() {
+    this.configService.getConfig()
+      .subscribe(
+        (data: Config) => this.config = { ...data }, // success path
+        error => this.error = error // error path
+      );
+  }
+
+  showConfigResponse() {
+  this.configService.getConfigResponse()
+    // resp is of type `HttpResponse<Config>`
+    .subscribe(resp => {
+      // display its headers
+      const keys = resp.headers.keys();
+      this.headers = keys.map(key =>
+        `${key}: ${resp.headers.get(key)}`);
+
+      // access the body directly, which is typed as `Config`.
+      this.config = { ... resp.body };
+    });
+}
+
+makeError() {
+    this.configService.makeIntentionalError().subscribe(null, error => this.error = error );
   }
 
 }
