2fcc87cc03dcd89d1dab2cb86676f701ad75cdba
NingGuo19 <ningguo19@gmail.com>
2019-11-01 17:49:29 +1100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { StudentService } from './student.service';
 import { StudentDisplayComponent } from './student-display/student-display.component';
 import { ConfigService } from './config/config.service';
 import { ConfigComponent } from './config/config.component';
+import { HeoresComponent } from './heores/heores.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, AppRoutingModule, HttpClientModule ],
-  declarations: [ AppComponent, HelloComponent, FeatureDisplayComponent, StudentDisplayComponent, ConfigComponent ],
+  declarations: [ AppComponent, HelloComponent, FeatureDisplayComponent, StudentDisplayComponent, ConfigComponent, HeoresComponent ],
   bootstrap:    [ AppComponent ],
   providers: [StudentService, ConfigService]
 })
